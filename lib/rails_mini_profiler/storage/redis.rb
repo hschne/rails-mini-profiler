f866bcaed787d1b0612f057407ebcb0adb0aa0d5
hschne <hans.schnedlitz@gmail.com>
2021-06-19 15:26:36 +0200

    Add bunch of redis stuff

--- lib/rails_mini_profiler/storage/redis.rb
+++ lib/rails_mini_profiler/storage/redis.rb
@@ -24,20 +24,21 @@ module RailsMiniProfiler
       end
 
       def find_by(user_id, **kwargs)
-
-        @redis.get(user_records_key(user_id))
-        result = @records[id.to_i]
-        raise(RecordNotFound, "Record with id='#{id}' found") unless result
-
-        result
+        user_records(user_id).select { | record| kwargs.all? { |k,v| record.public_send(k) == v } }
       end
 
-      def destroy(id)
-        @records.delete(id.to_i)
+      def destroy(user_id, id)
+        key = user_records_index(user_id)
+        @redis.lrem(key, id.to_i)
+        record_key = "#{@prefix}:#{@record_prefix}:#{record.id}"
+        @redis.del(record_key)
       end
 
-      def clear
-        @records.clear
+      def clear(user_id)
+        record_index_key = user_records_index(user_id)
+        record_keys = @redis.get(record_index_key).map { |id| "#{@prefix}:#{@record_prefix}:#{id}" }
+        @redis.del(*record_keys)
+        @redis.del(record_index_key)
       end
 
       private
@@ -61,11 +62,11 @@ module RailsMiniProfiler
       end
 
       def register_user_record(user_id, record_id)
-        key = user_records_key(user_id)
+        key = user_records_index(user_id)
         @redis.lpush(key, record_id)
         expired_items = @redis.lrange(key, @storage_limit, -1)
         @redis.del(expired_items)
-        @redis.ltrim(user_records_key, 0, @storage_limit - 1)
+        @redis.ltrim(user_records_index, 0, @storage_limit - 1)
       end
 
       def save_user_record(record)
@@ -73,11 +74,13 @@ module RailsMiniProfiler
         @redis.set(record_key, Marshal.dump(record))
       end
 
-      def user_record_keys(user_id)
+      def user_records(user_id)
+        record_keys = @redis.get(user_records_index(user_id)).map { |id| "#{@prefix}:#{@record_prefix}:#{id}" }
+        @redis.mget(record_keys).compact
       end
 
-      def user_records_key(user_id)
-        "#{@prefix}:#{@record_prefix}:#{user_id}:record-ids"
+      def user_records_index(user_id)
+        "#{@prefix}:#{@record_prefix}:#{user_id}:records-ids"
       end
     end
   end
