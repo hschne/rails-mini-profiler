b8a1b5b6e77f2da76a36edcd397126d5d93e59b3
hschne <hans.schnedlitz@gmail.com>
2021-08-01 09:14:47 +0200

    fix: add betters support for api-only applications, fixes #14

--- app/controllers/rails_mini_profiler/application_controller.rb
+++ app/controllers/rails_mini_profiler/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RailsMiniProfiler
-  class ApplicationController < ::ApplicationController
+  class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
     before_action :check_current_user
