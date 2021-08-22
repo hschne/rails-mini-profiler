c5093a008a2bbf7d6d6a4e22175d12b88fea5749
hschne <hans.schnedlitz@gmail.com>
2021-08-22 09:47:29 +0200

    build: update release workflow once again

--- lib/rails_mini_profiler/version.rb
+++ lib/rails_mini_profiler/version.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 module RailsMiniProfiler
-  # This needs to be double quoted for release-please
-  #
-  # See https://github.com/googleapis/release-please/issues/1018
-  VERSION = "0.3.0" # rubocop:disable Style/StringLiterals
+  VERSION = '0.3.0'
 end
