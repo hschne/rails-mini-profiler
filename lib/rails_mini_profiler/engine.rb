39dd858f6bf8adf0e9329a730d2b94a9f58f48ac
hschne <hans.schnedlitz@gmail.com>
2021-08-31 20:51:23 +0200

    fix: handle non-sprockets apps, fixes #55

--- lib/rails_mini_profiler/engine.rb
+++ lib/rails_mini_profiler/engine.rb
@@ -11,20 +11,24 @@ module RailsMiniProfiler
       app.middleware.use(RailsMiniProfiler::Middleware)
     end
 
-    initializer 'rails_mini_profiler.assets.precompile', group: :all do |app|
-      app.config.assets.precompile += %w[
-        rails_mini_profiler.js
-        rails_mini_profiler/application.css
-        vendor/assets/images
-      ]
-    end
-
     config.generators do |g|
       g.test_framework :rspec
     end
 
     initializer 'rails_mini_profiler_add_static assets' do |app|
       app.middleware.insert_before(ActionDispatch::Static, ActionDispatch::Static, "#{root}/public")
+    end
+
+    # If sprockets is not being used then there is no need to hook into asset compilation. Calling config.assets
+    # without Sprockets installed breaks compilation.
+    if defined?(Sprockets::Rails)
+      initializer 'rails_mini_profiler.assets.precompile', group: :all do |app|
+        app.config.assets.precompile += %w[
+          rails_mini_profiler.js
+          rails_mini_profiler/application.css
+          vendor/assets/images
+        ]
+      end
     end
   end
 end
