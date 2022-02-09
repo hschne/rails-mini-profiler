75b2bf192785349862e256e2cda577fb5f9ccf71
Andre Arko <andre@arko.net>
2022-02-09 15:02:08 -0800

    docs: update CONTRIBUTING for binstubs
    I guess Rails followed Ruby's lead and flattened bigint down into just integer?

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,7 +2,7 @@ Thanks for contributing to Rails Mini Profiler! 🎉
 
 Before submitting, please review the [contributor guidelines](/CONTRIBUTING.md). In particular:
 
-- [ ] Your tests still pass. Run `rake spec` to verify.
-- [ ] Your code adheres to the repository code style. Run `rake lint` to verify.
+- [ ] Your tests pass on all supported Rails versions. Run `bin/appraisal rspec` to verify.
+- [ ] Your code adheres to the repository code style. Run `bin/rake lint` to verify.
 - [ ] For functional updates, has the documentation been updated accordingly?
 
--- app/models/rails_mini_profiler/trace.rb
+++ app/models/rails_mini_profiler/trace.rb
@@ -5,12 +5,12 @@
 # Table name: rmp_traces
 #
 #  id                      :integer          not null, primary key
-#  rmp_profiled_request_id :bigint           not null
+#  rmp_profiled_request_id :integer          not null
 #  name                    :string
-#  start                   :bigint
-#  finish                  :bigint
+#  start                   :integer
+#  finish                  :integer
 #  duration                :integer
-#  allocations             :bigint
+#  allocations             :integer
 #  payload                 :json
 #  backtrace               :json
 #  created_at              :datetime         not null
