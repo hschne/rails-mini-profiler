308c0caf91c1d60b5fbb26301939e7490ec61874
Andre Arko <andre@arko.net>
2022-02-09 14:17:33 -0800

    test: use Rails-generated Events to test times

--- spec/lib/rails_mini_profiler/tracers/tracer_spec.rb
+++ spec/lib/rails_mini_profiler/tracers/tracer_spec.rb
@@ -6,9 +6,14 @@ module RailsMiniProfiler
   module Tracers
     RSpec.describe Tracer do
       describe 'trace' do
-        let(:event) { OpenStruct.new }
+        subject do
+          ActiveSupport::Notifications.subscribe('wait') do |*args|
+            @event = ActiveSupport::Notifications::Event.new(*args)
+          end
 
-        subject { Tracer.new(event) }
+          ActiveSupport::Notifications.instrument('wait') do
+            sleep 0.001
+          end
 
         it('should return a trace') do
           expect(subject.trace).to be_a(Trace)
@@ -19,6 +24,12 @@ module RailsMiniProfiler
           ms_duration = subject.trace.finish - subject.trace.start
           # if it's within 5, it must be the right order of magnitude
           expect(ms_duration).to be_within(5).of(subject.trace.duration)
+          Tracer.new(@event)
+        end
+
+        it('stores starts and ends as microseconds') do
+          micro_duration = subject.trace.finish - subject.trace.start
+          expect(micro_duration).to be_within(10).of(subject.trace.duration * 100)
         end
       end
     end
